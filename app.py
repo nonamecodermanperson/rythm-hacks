f211a80d4cc79997a6394044ea65d654701221aa
Shreyash Mishra <shreyashmishra2016@gmail.com>
2023-09-02 13:33:57 -0400

    finished up
    done

--- app.py
+++ app.py
@@ -32,6 +32,9 @@ def login():
             return render_template('login.html', error_message="Invalid username or password")
 
     return render_template('login.html')
+@app.route('/second-page')
+def second_page():
+    return render_template('second-page.html')  # Corrected template name
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
